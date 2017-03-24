7b1bdf448c2a45acd401ee1cafc68777c52133e7
Yoosuf <yoosuf290@yahoo.com>
2017-03-23 20:12:20 -0700

    initial commit

---
+++ CRCSolarTracker/CRCSolarTracker.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		CRCSolarTracker.ino
+ Created:	3/23/2017 8:02:43 PM
+ Author:	Yoosuf
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
